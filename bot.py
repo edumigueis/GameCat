27728a8e5c13e17f37b52559d571eb0a09d8671c
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2021-04-13 09:45:24 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 # bot.py
+# Made by Eduardo Migueis
+# 19167
 import os
 import random
 import requests
@@ -13,6 +15,9 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
 
 @bot.command(name='cat', brief='Picks a game from Steam for you. Args= [game_1], [game_2], ...', description='You can use the command without any arguments and get a random game from Steam. And you can put a list of games ([game_1], [game_2], ...) for the cat to pick one.')
 async def cat(ctx, *arg):
@@ -38,7 +43,8 @@ def get_prefix(author):
         'Well '+aut+', if you feel like having some fun: ',
         'Cats are connoisseurs of comfort. '+aut+', lay back and play: ',
         'I had you at meow, right? '+aut+' should play: ',
-        'Want a great recommendation? Here you go! '
+        'Want a great recommendation, '+aut+'? Here you go! ',
+        'I am a gamer cat! '+aut+'. Play:'
     ]
     return random.choice(cat_prefixes)
 
@@ -48,5 +54,10 @@ def get_random_game():
         "http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json")
     return response.json()['applist']['apps'][random.randint(0, 1000)]
 
+
+@bot.command(name='disclaimer', brief='A disclaimer about the bot.')
+async def disclaimer(ctx, *arg):
+    response = "Because this bot uses the Steam API with no restrictions to the content provided by Steam, the bot can recommend inappropriate content."
+    await ctx.send(response)
 
 bot.run(TOKEN)
